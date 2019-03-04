faccc11afbd73cc5f10ab81def36d574595805f7
pompidup <julienauvray28@gmail.com>
2019-03-04 12:59:37 +0100

    test

---
+++ primitives.js
@@ -0,0 +1,30 @@
+'use strict'
+
+/*
+ * Create a constant variables for each primitives
+ * `str`, `num`, `bool` and `undef`
+ *
+ * @notions Primitive and Operators, Variables
+ */
+
+// Your code :
+const string = 'Hello world'
+const number = 42
+const boolean = true
+const undefined
+
+//* Begin of tests
+import { strictEqual } from 'assert';
+
+strictEqual(typeof str, 'string')
+strictEqual(str, '1337')
+
+strictEqual(typeof num, 'number')
+strictEqual(num, 42)
+
+strictEqual(typeof bool, 'boolean')
+strictEqual(bool, false)
+
+strictEqual(typeof undef, 'undefined')
+strictEqual(undef, undefined)
+// End of tests */
